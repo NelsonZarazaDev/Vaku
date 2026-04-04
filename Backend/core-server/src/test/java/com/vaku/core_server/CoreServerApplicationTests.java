74df50dd9cc492bf46a96aaf60539b2790a946c6
Nelson Mauricio <nelsonnavarro@tmtek.net>
2026-04-03 22:45:23 -0500

    feat(core-server): Creation of the core microservice, along with its initial configuration

---
+++ Backend/core-server/src/test/java/com/vaku/core_server/CoreServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vaku.core_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoreServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
