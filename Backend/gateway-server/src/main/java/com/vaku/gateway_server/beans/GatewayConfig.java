9c17005e731b5baafc0d49af4ccd5b7ef61c62d2
Nelson Mauricio <nelsonnavarro@tmtek.net>
2026-04-07 23:22:50 -0500

    feat: Creating the blood type endpoint - Implementing the hexagonal architecture - List of all blood types

--- Backend/gateway-server/src/main/java/com/vaku/gateway_server/beans/GatewayConfig.java
+++ Backend/gateway-server/src/main/java/com/vaku/gateway_server/beans/GatewayConfig.java
@@ -17,6 +17,7 @@ public class GatewayConfig {
                 .route("department", r -> r.path("/core-server/department/**").uri("lb://core-server"))
                 .route("city", r -> r.path("/core-server/city/**").uri("lb://core-server"))
                 .route("documentType", r -> r.path("/core-server/documentType/**").uri("lb://core-server"))
+                .route("bloodType", r -> r.path("/core-server/bloodType/**").uri("lb://core-server"))
                 .build();
     }
 
